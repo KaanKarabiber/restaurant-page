ee39da9a2b2c90dbf97dc5a28de1caf39c745cf9
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2024-12-11 15:43:37 +0300

    add title to html add buttons to showHome apply background image and some css to home

--- src/home.js
+++ src/home.js
@@ -1,14 +1,26 @@
-import backgroundImgPath from "./assets/BackgroundImage.jpg";
-
 export function showHome(){
     const content = document.querySelector("#content");
     content.innerText = "";
-    const backgroundImage = document.createElement("img");
-    backgroundImage.src = backgroundImgPath;
+    
     const headline = document.createElement("h2");
-    headline.textContent = "Pizza Boys";
+    headline.textContent = "Pizza showtime";
+    
     const belowHeadlineText = document.createElement("p");
     belowHeadlineText.textContent = "Enjoy the finest dining experience with us.";
+
+    const reservationBttn = document.createElement("button");
+    reservationBttn.textContent = "Reservations";
+
+    const onlineBttn = document.createElement("button");
+    onlineBttn.textContent = "Order Online";
     
-    content.append(headline, belowHeadlineText, backgroundImage);
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+    buttonContainer.append(reservationBttn, onlineBttn);
+
+    const containerDiv = document.createElement("div");
+    containerDiv.classList.add("home-container");
+    containerDiv.append(headline, belowHeadlineText, buttonContainer);
+    
+    content.append(containerDiv);
 }
